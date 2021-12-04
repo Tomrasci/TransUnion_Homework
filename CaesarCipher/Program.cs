adecb3083b7ae717966d95e35a06bd5f49736fc0
Tomras2 <tomorune@gmail.com>
2021-12-04 18:07:40 +0200

    added input handling

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -7,10 +7,12 @@ namespace Caesar_Cipher
          static void Main(string[] args)
          {
             CaesarCipher cipher = new CaesarCipher();
+            try {
             System.Console.WriteLine("Please input a string to encrypt");
             string inputString = Console.ReadLine();
             System.Console.WriteLine("Please input the shift number");
             int shift = Convert.ToInt32(Console.ReadLine());
+            
             string encryptedMessage = cipher.Encrypt(inputString, shift);
             System.Console.WriteLine("--------------------------------------");
             System.Console.WriteLine("Encrypted message: " + encryptedMessage);
@@ -18,7 +20,10 @@ namespace Caesar_Cipher
             System.Console.WriteLine("--------------------------------------");
             System.Console.WriteLine("Decrypted message: " + decryptedMessage);
             System.Console.WriteLine("--------------------------------------");
-
+            }
+            catch (System.FormatException){
+            System.Console.WriteLine("Shift number must be a whole number");
+            }
 
          }
     }
